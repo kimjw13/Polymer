c00694246bc61a8dca31674485b63ac66247041e
Ace <bacotz@naver.com>
2017-03-09 16:00:19 +0900

    git Start!

---
+++ text2.java
@@ -0,0 +1,14 @@
+package text;
+import java.util.Scanner;
+public class text {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner number = new Scanner(System.in);
+		if(number.nextInt()==2)
+		{
+			System.out.println("ACE");
+		}
+	}
+
+}
